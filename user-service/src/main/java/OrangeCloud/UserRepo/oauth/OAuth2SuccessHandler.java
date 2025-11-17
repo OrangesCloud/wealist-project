d50f496c15d159ebf8a9c00c803cdff89ad84884
wonny <212clab@gmail.com>
2025-11-17 19:26:03 +0900

    -

--- user-service/src/main/java/OrangeCloud/UserRepo/oauth/OAuth2SuccessHandler.java
+++ user-service/src/main/java/OrangeCloud/UserRepo/oauth/OAuth2SuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import java.io.IOException;
 
 @Slf4j
