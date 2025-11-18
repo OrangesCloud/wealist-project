e9fce8f0b40292323e41cf12544c22cbdbd76962
wonny <212clab@gmail.com>
2025-11-18 18:51:58 +0900

    Update apiConfig.ts

--- frontend/src/api/apiConfig.ts
+++ frontend/src/api/apiConfig.ts
@@ -23,7 +23,7 @@ const getApiBaseUrl = (path: string): string => {
   return `https://api.wealist.co.kr${path}`;
 };
 
-export const USER_REPO_API_URL = getApiBaseUrl('/api');
+export const USER_REPO_API_URL = getApiBaseUrl('/api/users');
 export const BOARD_SERVICE_API_URL = getApiBaseUrl('/api/boards/api');
 
 // ============================================================================
