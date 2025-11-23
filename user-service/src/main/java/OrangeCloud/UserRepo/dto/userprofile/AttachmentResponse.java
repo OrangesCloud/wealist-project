ba8907fef168bfa463d1162b15fc208bd5332217
ressKim <ressK4149@gmail.com>
2025-11-23 21:09:07 +0900

    직렬화 문제 수정

--- user-service/src/main/java/OrangeCloud/UserRepo/dto/userprofile/AttachmentResponse.java
+++ user-service/src/main/java/OrangeCloud/UserRepo/dto/userprofile/AttachmentResponse.java
@@ -1,9 +1,12 @@
 package OrangeCloud.UserRepo.dto.userprofile;
 
 import OrangeCloud.UserRepo.entity.Attachment;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -12,7 +15,10 @@ import java.util.UUID;
  */
 @Getter
 @Builder
-public class AttachmentResponse {
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttachmentResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
     private UUID attachmentId;
     private String entityType;
     private UUID entityId;
