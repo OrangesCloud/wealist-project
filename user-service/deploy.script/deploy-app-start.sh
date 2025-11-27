a793a472c2013d0341f85924db92e5a5277b61e3
wonny <212clab@gmail.com>
2025-11-27 15:55:50 +0900

    Update deploy-app-start.sh

--- user-service/deploy.script/deploy-app-start.sh
+++ user-service/deploy.script/deploy-app-start.sh
@@ -96,9 +96,9 @@ export BOARD_DB_PASSWORD=$(load_secret "db/board_db_password")
 
 
 # 되었으면 소망을 담는다
-export GOOGLE_CLIENT_ID=640996696843-gtht74dpnn9c4u7mb6k5craur1vojgbk.apps.googleusercontent.com
+export GOOGLE_CLIENT_ID="640996696843-gtht74dpnn9c4u7mb6k5craur1vojgbk.apps.googleusercontent.com"
 export GOOGLE_CLIENT_SECRET=$(load_secret "oauth/google_client_secret")
-export OAUTH2_REDIRECT_URL=http://wealist.co.kr/oauth/callback
+export OAUTH2_REDIRECT_URL="http://wealist.co.kr/oauth/callback"
 
 
 
